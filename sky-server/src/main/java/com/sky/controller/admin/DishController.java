b905062c4e653004f9659862543eb8194bcaf7ca
Vino <565214822@qq.com>
2024-01-30 22:01:02 +0800

    2024/01/30： (1)完成C端“缓存菜品”功能

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -12,7 +12,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @Api(tags = "菜品相关接口")
 @Slf4j
@@ -22,6 +25,8 @@ public class DishController {
 
     @Autowired
     DishService dishService;
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
 
     /**
      * 菜品分页查询
@@ -54,6 +59,10 @@ public class DishController {
     @ApiOperation("修改菜品")
     @PutMapping
     public Result edit(@RequestBody DishDTO dishDTO) {
+        //对菜品的修改操作需要删除缓存
+        //这里存在一个问题，如果修改了当前菜品的“分类信息”，就会牵扯到两个分类，此时如果要精准删除这两个分类的缓存，就需要去数据库进行查询
+        //在分类不多的情况下，可以考虑清除全部缓存
+        cleanCache("category_*");
         dishService.edit(dishDTO);
         return Result.success();
     }
@@ -66,6 +75,7 @@ public class DishController {
     @ApiOperation("新增菜品")
     @PostMapping
     public Result add(@RequestBody DishDTO dishDTO) {
+        //新增菜品时，由于新增的菜品默认处于 “停售” 状态，所以在新增时无需删除缓存，当新菜品“启售”时，再删除缓存即可
         dishService.add(dishDTO);
         return Result.success();
     }
@@ -78,6 +88,7 @@ public class DishController {
     @ApiOperation("删除菜品")
     @DeleteMapping
     public Result delete(String ids) {
+        //删除菜品时，由于 “启售” 状态的菜品无法删除，删除的都是 “停售” 的菜品，所以这里也不需要删除缓存
         if(BaseContext.getCurrentId() != 1) {
             return Result.error(MessageConstant.AINT_ADMIN);
         }
@@ -101,8 +112,21 @@ public class DishController {
     @ApiOperation("起售、停售菜品")
     @PostMapping("/status/{status}")
     public Result enableOrDisable(Long id, @PathVariable Integer status) {
+        //启售、停售菜品时，需要删除对应的缓存，为了方便，将所有的缓存一并清理掉
+        cleanCache("category_*");
         dishService.enableOrDisable(id, status);
         return Result.success();
+    }
+
+    /**
+     * 根据 pattern 删除 redis 缓存，保持数据一致性
+     * @param pattern 样式
+     */
+    private void cleanCache(String pattern) {
+        Set<String> keys = redisTemplate.keys(pattern);
+        if(keys != null) {
+            redisTemplate.delete(keys);
+        }
     }
 
 }
